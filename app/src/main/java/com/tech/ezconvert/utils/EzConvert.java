c9fd9ab397c344fb9f3e8ac46903740eddeba274
xiaohao <198413060+XiaoHao560@users.noreply.github.com>
2026-03-12 00:03:23 +0800

    feat: 添加崩溃报告

---
+++ app/src/main/java/com/tech/ezconvert/utils/EzConvert.java
@@ -0,0 +1,14 @@
+package com.tech.ezconvert.utils;
+
+import android.app.Application;
+
+public class EzConvert extends Application {
+    
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        
+        // 初始化崩溃捕获
+        CrashHandler.getInstance().init(this);
+    }
+}
