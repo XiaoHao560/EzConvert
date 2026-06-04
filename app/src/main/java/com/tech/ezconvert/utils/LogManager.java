5d91f0d913bdc046d9dd3c8c56728c864bafef6a
xiaohao <198413060+XiaoHao560@users.noreply.github.com>
2026-06-05 07:18:25 +0800

    chore: 修改Log使用

--- app/src/main/java/com/tech/ezconvert/utils/LogManager.java
+++ app/src/main/java/com/tech/ezconvert/utils/LogManager.java
@@ -218,7 +218,7 @@ public class LogManager {
             notifyLogAdded(entry);
             
         } catch (Throwable e) {
-            android.util.Log.e(TAG, "addAppLog异常", e);
+            Log.e(TAG, "addAppLog异常", e);
         }
     }
 
@@ -359,7 +359,7 @@ public class LogManager {
             writer.flush();
             writer.close();
         } catch (IOException e) {
-            android.util.Log.e(TAG, "写入失败: " + e.getMessage());
+            Log.e(TAG, "写入失败: " + e.getMessage());
         }
     }
 
@@ -370,7 +370,7 @@ public class LogManager {
             writer.flush();
             writer.close();
         } catch (IOException e) {
-            android.util.Log.e(TAG, "清空失败: " + e.getMessage());
+            Log.e(TAG, "清空失败: " + e.getMessage());
         }
     }
 
